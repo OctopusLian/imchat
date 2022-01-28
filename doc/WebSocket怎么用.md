af2d49af02d7ae881093e2e2222c6b2bdc75a8f7
OctopusLian <zoctopus@qq.com>
2022-01-28 20:07:38 +0800

    登录逻辑实现

--- doc/WebSocket怎么用.md
+++ doc/WebSocket怎么用.md
@@ -3,5 +3,7 @@
  * @Author: neozhang
  * @Date: 2021-12-28 22:44:38
  * @LastEditors: neozhang
- * @LastEditTime: 2021-12-28 22:47:19
+ * @LastEditTime: 2022-01-28 19:44:44
 -->
+
+## 接入鉴权/conn  
--- main.go
+++ main.go
@@ -3,19 +3,18 @@
  * @Author: neozhang
  * @Date: 2022-01-10 22:21:55
  * @LastEditors: neozhang
- * @LastEditTime: 2022-01-10 22:41:46
+ * @LastEditTime: 2022-01-28 20:06:11
  */
 package main
 
 import (
-	"io"
 	"net/http"
 )
 
 func main() {
 	//绑定请求和处理函数
 	http.HandleFunc("/user/login",
-		func(rw http.ResponseWriter, r *http.Request) {
+		func(writer http.ResponseWriter, request *http.Request) {
 			//数据库操作
 			//逻辑处理
 			//restapi json/xml返回
@@ -25,21 +24,24 @@ func main() {
 			//如何获得参数
 			//解析参数
 			request.ParseForm()
-			mobile := request.PostForm().Get
-			passwd
+			mobile := request.PostForm.Get("mobile")
+			passwd := request.PostForm.Get("passwd")
 
 			loginok := false
-			if mobile == "18600000000" && passwd == “123456” {
-				lologinok = true
+			if mobile == "18600000000" && passwd == "123456" {
+				loginok = true
 			}
 
 			//如何返回json
-			if loginok {
-				//返回json ok
-			} else {
-				//返回json failed
+			str := `{"code":0,"msg":"{"id":1,"token":"test"}}"`
+			if !loginok {
+				str = `{"code":0,"data":"密码不正确"`
 			}
-			//io.WriteString(rw, "hello world")
+			//设置header为JSON默认的text/html
+			writer.Header().Set("Content-Type", "application/json")
+			writer.WriteHeader(http.StatusOK)
+			writer.Write([]byte(str))
+
 		})
 	//启动web服务器
 	http.ListenAndServe(":8080", nil)
